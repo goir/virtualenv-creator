3d008fcd003f296d3a443218460997dc6c03619d
Goir <goir@users.noreply.github.com>
2015-12-22 16:00:37 +0100

    update setuptools from 18.7.1 to 19.1.1

--- create_virtualenv.py
+++ create_virtualenv.py
@@ -13,7 +13,7 @@ import urllib2
 import shutil
 
 WHEEL_PIP = 'https://pypi.python.org/packages/py2.py3/p/pip/pip-7.1.2-py2.py3-none-any.whl#md5=5ff9fec0be479e4e36df467556deed4d'
-WHEEL_SETUPTOOLS = 'https://pypi.python.org/packages/3.5/s/setuptools/setuptools-18.7.1-py2.py3-none-any.whl#md5=a793e7b7ad3a91ca796f2e3bbd4ac355'
+WHEEL_SETUPTOOLS = 'https://pypi.python.org/packages/3.5/s/setuptools/setuptools-19.1.1-py2.py3-none-any.whl#md5=29d589434b067e67c95d6625a211909e'
 
 
 class Colors(object):
